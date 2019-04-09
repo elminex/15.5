eed5565fac2bfd3559df4b6941e464a4a6075a22
Paweł Tuliński <pawel.tulinski@gmail.com>
2019-04-09 22:23:31 +0200

    Finish task

---
+++ js/script.js
@@ -0,0 +1,21 @@
+function Button(text) {
+    this.text = text || 'Hello';
+}
+Button.prototype = {
+    create: function () {
+        let self = this;
+        this.element = document.createElement('button');
+        this.element.innerText = this.text;
+        this.element.addEventListener('click', function () {
+            alert(self.text);
+        });
+
+//        this.element.addEventListener('click', () => {
+//            alert(this.text);
+//        });
+
+        document.body.appendChild(this.element);
+    }
+}
+let btn1 = new Button('hello');
+btn1.create();
